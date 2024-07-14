f18504ded0f9072e9313886452f258e6cc983e61
Henrry <Henrry@GOPHERPAD>
2024-07-14 01:39:29 -0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SQLite;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AgenciaSeguros
+{
+  internal static class Program
+  {
+    /// <summary>
+    /// The main entry point for the application.
+    /// </summary>
+    [STAThread]
+    static void Main()
+    {
+      // Especifica el nombre y la ubicación de la base de datos
+      var databaseFile = "AgenciaSeguros.db";
+
+      // Verifica si el archivo de la base de datos ya existe
+      if (!File.Exists(databaseFile))
+      {
+        // Si no existe, crea el archivo de la base de datos
+        SQLiteConnection.CreateFile(databaseFile);
+        Console.WriteLine($"Base de datos '{databaseFile}' creada exitosamente.");
+      }
+      else
+      {
+        Console.WriteLine($"La base de datos '{databaseFile}' ya existe.");
+      }
+      Application.EnableVisualStyles();
+      Application.SetCompatibleTextRenderingDefault(false);
+      Application.Run(new Form1());
+    }
+  }
+}
